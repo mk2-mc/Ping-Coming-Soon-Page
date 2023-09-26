9f01f21a3c00ece32f6ce1a35d3f1863b2b934d9
Mk.2 <55669745+mk2-mc@users.noreply.github.com>
2023-09-26 01:35:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ping Coming Soon Page Solution
+
+This is a solution to the [Ping coming soon page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+# Built with
+
+- HTML
+- CSS
+- JavaScript
---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".form");
+const emailInput = document.querySelector(".email-input");
+const errorText = document.querySelector(".error-text");
+const confirmation = document.querySelector(".confirmation");
+
+function validate() {
+  const emailFormat = /^\w+([\.-]?\w+)a*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  
+  if ((!emailFormat.test(emailInput.value))
+  || (emailInput.value === "")) {
+    emailInput.classList.add("error-input");
+    errorText.classList.remove("hidden");
+    errorText.innerHTML = "Please provide a valid email address";
+    
+    return false;
+  }
+  
+  return true;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  if (validate()) {
+    form.classList.add("hidden");
+    confirmation.classList.remove("hidden");
+  }
+});
