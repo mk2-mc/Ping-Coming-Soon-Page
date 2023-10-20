5b2da7966bc841accda8ca39e544b3006b9e2b7e
Mk.2 <55669745+mk2-mc@users.noreply.github.com>
2023-10-20 17:00:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const confirmation = document.querySelector(".confirmation");
 function validate() {
   const emailFormat = /^\w+([\.-]?\w+)a*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   
-  if (!emailFormat.test(emailInput.value)) {
+  if (!emailFormat.test(emailInput.value.trim())) {
     emailInput.classList.add("error-input");
     errorText.classList.remove("hidden");
     errorText.innerHTML = "Please provide a valid email address";
